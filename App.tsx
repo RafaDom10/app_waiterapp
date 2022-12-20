3f536c4dec72e473b8b903fa40385a4e0cb94ed0
Rafael Domingues <r-domingues@outlook.com>
2022-12-19 22:05:45 -0300

    feat: added menu list

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { Main } from './src/Main'
 
-export default function App ():  | null {
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
+export default function App () {
   const [isFontsLoaded] = useFonts({
     'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
     'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
